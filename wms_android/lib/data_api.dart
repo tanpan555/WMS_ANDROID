37683357693c3d2acabafb8aeb6bba5a7d173b72
its-saksri_s <its-saksri_s@internal.softsquaregroup.com>
2024-08-15 14:32:59 +0700

    รวมกับของพี่ที่พึง push มา และ api session

--- wms_android/lib/data_api.dart
+++ wms_android/lib/data_api.dart
@@ -15,22 +15,13 @@ class ApiService {
     if (response.statusCode == 200) {
       try {
         // Decode the response body to JSON
-        final responseBody = utf8.decode(response
-            .bodyBytes); // Ensure proper decoding for non-ASCII characters
+        final responseBody = utf8.decode(response.bodyBytes);
         final data = jsonDecode(responseBody);
         print(data); // Decode JSON
 
         globals.sessionid = data['items'][0]['sessionid'];
-        // globals.sessionid = sessionIdNumber.toString();
         final app_session = globals.sessionid;
-
-        print(globals.sessionid);
-        print(
-          'browser_language : $browser_language Type : ${browser_language.runtimeType}');
-      print('app_user : $app_user Type : ${app_user.runtimeType}');
-      print('app_session : $app_session Type : ${app_session.runtimeType}');
-
-        upSessionid(app_session);
+        updateSessionId(app_session);
       } catch (e) {
         print('Error decoding JSON: $e');
       }
@@ -39,31 +30,26 @@ class ApiService {
     }
   }
 
-  Future<void> upSessionid(app_session) async {
+  Future<void> updateSessionId(int app_session) async {
     final response = await http.get(Uri.parse(
         'http://172.16.0.82:8888/apex/wms/c/sessionid_update/$browser_language/$app_user/$app_session'));
-        print(
-          'browser_language : $browser_language Type : ${browser_language.runtimeType}');
-      print('app_user : $app_user Type : ${app_user.runtimeType}');
-      print('app_session : $app_session Type : ${app_session.runtimeType}');
 
     if (response.statusCode == 200) {
       try {
-        // Decode the response body to JSON
-        final responseBody = utf8.decode(response
-            .bodyBytes); // Ensure proper decoding for non-ASCII characters
-        final data = jsonDecode(responseBody);
-        print(data); // Decode JSON
+        final responseBody = utf8.decode(response.bodyBytes);
+        print('Response Body: $responseBody'); // ตรวจสอบข้อมูลที่ได้รับ
+        if (responseBody.isNotEmpty) {
+          final data = jsonDecode(responseBody);
+          final dataSessionId = data['app_session'];
+          globals.APP_SESSION = int.parse(dataSessionId);
+        } else {
+          print('Empty response body.');
+        }
       } catch (e) {
         print('Error decoding JSON: $e');
       }
     } else {
-      print('Failed to load data');
-      print(
-          'browser_language : $browser_language Type : ${browser_language.runtimeType}');
-      print('app_user : $app_user Type : ${app_user.runtimeType}');
-      print('app_session : $app_session Type : ${app_session.runtimeType}');
-      print(response.statusCode);
+      print('updateSessionId Failed to load data: ${response.statusCode}');
     }
   }
 }
