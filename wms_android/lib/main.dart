3f5c694d3c63d93134a58e59c4341c256f6cf089
its-saksri_s <its-saksri_s@internal.softsquaregroup.com>
2024-11-15 10:22:16 +0700

    แก้ไขการ check data ในหน้า hme, menuLV2 เพิ่มการกันไม่ให้กดปุมหรือ card ซ้อนกัน SSFGDT09L เพิ่ม eventButton(Next page) กลาง ใน Styles.dart

--- wms_android/lib/main.dart
+++ wms_android/lib/main.dart
@@ -7,7 +7,7 @@ import 'login.dart';
 import 'bottombar.dart';
 import 'package:wms_android/Global_Parameter.dart' as globals;
 import 'loading.dart';
-import 'centered_message.dart';
+// import 'centered_message.dart';
 
 void main() {
   runApp(const MyApp());
@@ -137,118 +137,115 @@ class _MyHomePageState extends State<MyHomePage> {
             Expanded(
               child: isLoading
                   ? Center(child: LoadingIndicator())
-                  : dataMenu.isEmpty
-                      ? const Center(child: CenteredMessage())
-                      : SingleChildScrollView(
-                          child: GridView.builder(
-                            shrinkWrap: true,
-                            physics: NeverScrollableScrollPhysics(),
-                            gridDelegate:
-                                SliverGridDelegateWithFixedCrossAxisCount(
-                              crossAxisCount: 2,
-                              crossAxisSpacing: 5,
-                              mainAxisSpacing: 5,
-                              childAspectRatio: 1.0,
-                            ),
-                            itemCount: dataMenu.length,
-                            itemBuilder: (context, index) {
-                              final item = dataMenu[index];
+                  // : dataMenu.isEmpty
+                  //     ? const Center(child: CenteredMessage())
+                  : SingleChildScrollView(
+                      child: GridView.builder(
+                        shrinkWrap: true,
+                        physics: NeverScrollableScrollPhysics(),
+                        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                          crossAxisCount: 2,
+                          crossAxisSpacing: 5,
+                          mainAxisSpacing: 5,
+                          childAspectRatio: 1.0,
+                        ),
+                        itemCount: dataMenu.length,
+                        itemBuilder: (context, index) {
+                          final item = dataMenu[index];
 
-                              Color? cardColor;
-                              String imagePath;
-                              String p_attr1;
-                              String p_ou_code;
+                          Color? cardColor;
+                          String imagePath;
+                          String p_attr1;
+                          String p_ou_code;
 
-                              switch (item['card_value']) {
-                                case 'WMS คลังวัตถุดิบ':
-                                  imagePath = 'assets/images/open-box2.png';
-                                  cardColor = Colors.green[200];
-                                  p_attr1 = globals.Raw_Material;
-                                  p_ou_code = globals.P_ERP_OU_CODE;
-                                  break;
-                                case 'WMS คลังสำเร็จรูป':
-                                  imagePath = 'assets/images/box1.png';
-                                  cardColor = Colors.blue[200];
-                                  p_attr1 = globals.Finishing;
-                                  p_ou_code = globals.P_ERP_OU_CODE;
-                                  break;
-                                case 'พิมพ์ Tag':
-                                  imagePath =
-                                      'assets/images/barcode-scanner.png';
-                                  cardColor = Colors.pink[100];
-                                  p_attr1 = '';
-                                  p_ou_code = globals.P_ERP_OU_CODE;
-                                  break;
-                                case 'ตรวจนับประจำงวด':
-                                  imagePath = 'assets/images/open-box2.png';
-                                  cardColor = Colors.orange[200];
-                                  p_attr1 = globals.Raw_Material;
-                                  p_ou_code = globals.P_ERP_OU_CODE;
-                                  break;
-                                // Add more cases as needed
-                                default:
-                                  imagePath = 'assets/images/open-box2.png';
-                                  cardColor = Colors.grey;
-                                  p_attr1 = '';
-                                  p_ou_code = globals.P_ERP_OU_CODE;
+                          switch (item['card_value']) {
+                            case 'WMS คลังวัตถุดิบ':
+                              imagePath = 'assets/images/open-box2.png';
+                              cardColor = Colors.green[200];
+                              p_attr1 = globals.Raw_Material;
+                              p_ou_code = globals.P_ERP_OU_CODE;
+                              break;
+                            case 'WMS คลังสำเร็จรูป':
+                              imagePath = 'assets/images/box1.png';
+                              cardColor = Colors.blue[200];
+                              p_attr1 = globals.Finishing;
+                              p_ou_code = globals.P_ERP_OU_CODE;
+                              break;
+                            case 'พิมพ์ Tag':
+                              imagePath = 'assets/images/barcode-scanner.png';
+                              cardColor = Colors.pink[100];
+                              p_attr1 = '';
+                              p_ou_code = globals.P_ERP_OU_CODE;
+                              break;
+                            case 'ตรวจนับประจำงวด':
+                              imagePath = 'assets/images/open-box2.png';
+                              cardColor = Colors.orange[200];
+                              p_attr1 = globals.Raw_Material;
+                              p_ou_code = globals.P_ERP_OU_CODE;
+                              break;
+                            // Add more cases as needed
+                            default:
+                              imagePath = 'assets/images/open-box2.png';
+                              cardColor = Colors.grey;
+                              p_attr1 = '';
+                              p_ou_code = globals.P_ERP_OU_CODE;
+                          }
+
+                          return GestureDetector(
+                            onTap: () {
+                              if (p_attr1 == globals.Raw_Material) {
+                                globals.ATTR1 = 'Raw Material';
+                              } else if (p_attr1 == globals.Finishing) {
+                                globals.ATTR1 = 'Finishing';
                               }
-
-                              return GestureDetector(
-                                onTap: () {
-                                  if (p_attr1 == globals.Raw_Material) {
-                                    globals.ATTR1 = 'Raw Material';
-                                  } else if (p_attr1 == globals.Finishing) {
-                                    globals.ATTR1 = 'Finishing';
-                                  }
-                                  Navigator.push(
-                                    context,
-                                    MaterialPageRoute(
-                                      builder: (context) => TestMenuLv2(
-                                        menu_id: item['menu_id'],
-                                        sessionID: SessionManager().sessionID,
-                                        p_attr1: p_attr1,
-                                        p_ou_code: p_ou_code,
-                                      ),
-                                    ),
-                                  );
-                                },
-                                child: Card(
-                                  elevation: 4.0,
-                                  margin: const EdgeInsets.symmetric(
-                                      vertical: 5, horizontal: 5),
-                                  color: cardColor, // Set card color
-                                  shape: RoundedRectangleBorder(
-                                    borderRadius: BorderRadius.circular(
-                                        10), // Adjust border radius
-                                  ),
-                                  child: Padding(
-                                    padding: const EdgeInsets.all(
-                                        15.0), // Add padding
-                                    child: Column(
-                                      mainAxisAlignment:
-                                          MainAxisAlignment.center,
-                                      children: [
-                                        Image.asset(
-                                          imagePath, // Use imagePath from switch
-                                          width: 70, // Set image size
-                                        ),
-                                        const SizedBox(height: 10),
-                                        Text(
-                                          item['card_value'] ?? 'No Name',
-                                          style: const TextStyle(
-                                            fontSize: 12,
-                                            color: Colors.black,
-                                            fontWeight: FontWeight.bold,
-                                          ),
-                                        ),
-                                      ],
-                                    ),
+                              Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                  builder: (context) => TestMenuLv2(
+                                    menu_id: item['menu_id'],
+                                    sessionID: SessionManager().sessionID,
+                                    p_attr1: p_attr1,
+                                    p_ou_code: p_ou_code,
                                   ),
                                 ),
                               );
                             },
-                          ),
-                        ),
+                            child: Card(
+                              elevation: 4.0,
+                              margin: const EdgeInsets.symmetric(
+                                  vertical: 5, horizontal: 5),
+                              color: cardColor, // Set card color
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(
+                                    10), // Adjust border radius
+                              ),
+                              child: Padding(
+                                padding:
+                                    const EdgeInsets.all(15.0), // Add padding
+                                child: Column(
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                  children: [
+                                    Image.asset(
+                                      imagePath, // Use imagePath from switch
+                                      width: 70, // Set image size
+                                    ),
+                                    const SizedBox(height: 10),
+                                    Text(
+                                      item['card_value'] ?? 'No Name',
+                                      style: const TextStyle(
+                                        fontSize: 12,
+                                        color: Colors.black,
+                                        fontWeight: FontWeight.bold,
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                              ),
+                            ),
+                          );
+                        },
+                      ),
+                    ),
             ),
           ],
         ),
