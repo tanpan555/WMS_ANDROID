352c4d684a90037bcfae6c0c0ca6c677537580a1
SOFTSQUAREGROUP\pirachat_y <pirachat_y@softsquaregroup.com>
2025-03-25 09:11:14 +0700

    no message

---
+++ wms_android/lib/Global_ConfirmDialog.dart
@@ -0,0 +1,79 @@
+import 'package:flutter/material.dart';
+
+Future<bool?> showConfirmationDialog({
+  required BuildContext context,
+  required String title,
+  required String content,
+  String cancelText = 'ยกเลิก',
+  String confirmText = 'ตกลง',
+}) async {
+  return showDialog<bool>(
+    context: context,
+    barrierDismissible: false,
+    builder: (BuildContext context) {
+      return Dialog(
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(0)),
+        child: Container(
+          decoration: BoxDecoration(
+            border: Border.all(
+                color: const Color.fromARGB(255, 138, 135, 137), width: 2.0),
+            borderRadius: BorderRadius.circular(0),
+          ),
+          child: Padding(
+            padding: const EdgeInsets.all(25.0),
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Text(
+                  title,
+                  style: const TextStyle(
+                      fontSize: 24,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.black),
+                  textAlign: TextAlign.center,
+                ),
+                const SizedBox(height: 20),
+                Text(
+                  content,
+                  style: const TextStyle(fontSize: 20, color: Colors.black),
+                  textAlign: TextAlign.center,
+                ),
+                const SizedBox(height: 20),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: const Color.fromARGB(255, 191, 57, 57), // ใช้สีที่กำหนด
+                        elevation: 0,
+                      ),
+                      onPressed: () => Navigator.of(context).pop(false),
+                      child: Text(
+                        cancelText,
+                        style:
+                            const TextStyle(color: Colors.white, fontSize: 20),
+                      ),
+                    ),
+                    ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: Colors.green, // ใช้สีที่กำหนด
+                        elevation: 0,
+                      ),
+                      onPressed: () => Navigator.of(context).pop(true),
+                      child: Text(
+                        confirmText,
+                        style:
+                            const TextStyle(color: Colors.white, fontSize: 20),
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
+        ),
+      );
+    },
+  );
+}
